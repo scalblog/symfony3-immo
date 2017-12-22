35a71b9cafcfbe3868462bf27b53910d93bd7488
scalblog <scalblog@yahoo.fr>
2017-12-22 13:29:14 +0100

    new slugs added

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -9,6 +9,26 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller 
 {
+    //La route fait appel à OCPlatformBundle:Advert:view,
+    //on doit donc définir la méthode viewAction.
+    //On donne à cette méthode l'argument $id, pour
+    //correspondre au paramètre {id} de la route.
+
+    public function viewAction($id){
+        // $id vaut 5 si on appelle l'URL platform/advert/5
+        // Ici on récupérera depuis la base de données
+        // l annonce correspondante à l id $id
+        // Puis on passera l'annonce à la vue pour
+        // qu'elle l affiche
+
+        return new Response("Affichage de l'annonce d'id : ".$id);
+    }
+
+    public function viewSlugAction($slug, $year, $format){
+        return new Response("
+            On pourrait afficher l'annonce correspondant au slug '".$slug."', créée en ".$year." et au format ".$format."."
+        );
+    }
 
     public function indexAction() {
         $content = $this
