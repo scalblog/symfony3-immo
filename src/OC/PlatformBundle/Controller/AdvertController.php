d58d72bf9555ed35f6ffb7f4c8083805f72c3219
scalblog <scalblog@yahoo.fr>
2017-12-23 18:00:34 +0100

    test session and flashbag

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -3,6 +3,7 @@
 namespace OC\PlatformBundle\Controller;
 // N'oubliez pas ce use :
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 class AdvertController extends Controller
 {
@@ -18,13 +19,24 @@ class AdvertController extends Controller
   }
   // La route fait appel à OCPlatformBundle:Advert:view, on doit donc définir la méthode viewAction.
   // On donne à cette méthode l'argument $id, pour correspondre au paramètre {id} de la route
-  public function viewAction($id)
+  public function viewAction($id, Request $request)
   {
+    // on récupère le paramètre tag:
+    $tag = $request->query->get('tag');
     // $id vaut 5 si l'on a appelé l'URL /platform/advert/5
     // Ici, on récupèrera depuis la base de données l'annonce correspondant à l'id $id.
     // Puis on passera l'annonce à la vue pour qu'elle puisse l'afficher
-    return new Response("Affichage de l'annonce d'id : ".$id);
+    return $this->render('OCPlatformBundle:Advert:view.html.twig', array('id'=>$id));
   }
+
+public function addAction(Request $request)
+{
+    $session = $request->getSession();
+    $session->getFlashBag()->add('info', 'Annnonce bien enregistrée');
+    $session->getFlashBag()->add('info', 'Oui, oui, l annnonce bien enregistrée');
+    return $this->redirectToRoute('oc_platform_view', array('id'=>5));
+}
+
   // On récupère tous les paramètres en arguments de la méthode
   public function viewSlugAction($slug, $year, $_format)
   {
